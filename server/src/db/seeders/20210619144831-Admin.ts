d8b6c62893fe0ce29e0899d570289ef27910c1eb
Pouya <pouya.mozafar@gmail.com>
2021-06-22 01:02:05 +0430

    Add seed of admin

--- server/src/db/seeders/20210619144831-Admin.ts
+++ server/src/db/seeders/20210619144831-Admin.ts
@@ -1,9 +1,9 @@
-import Sequelize, { QueryInterface } from 'sequelize';
+import { QueryInterface } from 'sequelize';
 import encrypt from '../../shared/utilities/encrypt';
-import { ADMIN_TABLE_NAME, BLACK_LISTS_TABLE_NAME } from '../shared/tableNames';
+import { ADMIN_TABLE_NAME } from '../shared/tableNames';
 
 export default {
-  up: async (queryInterface: QueryInterface, sq: typeof Sequelize): Promise<void> => {
+  up: async (queryInterface: QueryInterface): Promise<void> => {
     await queryInterface.bulkInsert(ADMIN_TABLE_NAME, [{
       username: 'admin',
       password: encrypt('admin'),
